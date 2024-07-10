d33de5be8d59313d9714586834f92d57485da20f
Petr Erastov <erapetos@gmail.com>
2024-07-10 18:37:48 +0300

    Extend node roles

--- cybsi/__version__.py
+++ cybsi/__version__.py
@@ -1,4 +1,4 @@
-__version__ = "2.12.3"
+__version__ = "2.13.0a1"
 __title__ = "cybsi-python-sdk"
 __description__ = "Cybersecurity threat intelligence development kit"
 __license__ = "Apache License 2.0"
